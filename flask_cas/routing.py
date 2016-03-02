7d77414b0b6c6ee7cbb11b443070c0727af481b8
Ben Steinberg <benjamin_steinberg@harvard.edu>
2016-03-02 12:55:41 -0500

    Wrap logging in try-except

--- flask_cas/routing.py
+++ flask_cas/routing.py
@@ -67,8 +67,11 @@ def logout():
     cas_username_session_key = current_app.config['CAS_USERNAME_SESSION_KEY']
     cas_attributes_session_key = current_app.config['CAS_ATTRIBUTES_SESSION_KEY']
 
-    pp = pprint.PrettyPrinter(indent=0)
-    current_app.logger.info("User logged out: %s" % (pp.pformat(flask.session[cas_attributes_session_key]).replace("\n", " "),))
+    try:
+        pp = pprint.PrettyPrinter(indent=0)
+        current_app.logger.info("User logged out: %s" % (pp.pformat(flask.session[cas_attributes_session_key]).replace("\n", " "),))
+    except:
+        pass
 
     if cas_username_session_key in flask.session:
         del flask.session[cas_username_session_key]
